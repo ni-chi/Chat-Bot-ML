b0b12f2525fd00e6bfd8383654e8c4958156d958
Nihar Chitnis <niharchitnis@gmail.com>
2018-03-12 23:50:18 +0530

    First Commit

---
+++ setup.sh
@@ -0,0 +1,7 @@
+sudo apt-get install python-pyaudio python3-pyaudio
+sudo apt-get install portaudio19-dev python-all-dev python3-all-dev && sudo pip3 install pyaudio
+sudo pip3 install pyttsx3
+sudo pip3 install google-api-python-client
+sudo pip3 install monotonic
+sudo pip3 install SpeechRecognition
+sudo pip3 install keyboard
